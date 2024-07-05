7eac0df9d10be76a2c28f03fe9a765626c75f9e0
David Ruiz <jesuruga@gmail.com>
2024-07-05 00:39:12 -0600

    [FIX] token jwt

--- src/adapters/controllers/usuarioController.ts
+++ src/adapters/controllers/usuarioController.ts
@@ -22,7 +22,7 @@ export const loginUsuario = async (req: Request, res: Response) => {
     if (!usuario || usuario.contraseña !== contraseña) {
       return res.status(401).send({ error: 'Credenciales no válidas.' });
     }
-    const token = jwt.sign({ _id: usuario._id }, process.env.JWT_SECRET || 'your_secret_key', { expiresIn: '1h' });
+    const token = jwt.sign({ _id: usuario._id }, process.env.JWT_SECRET || 'your_secret_key',);
     res.send({ usuario, token });
   } catch (error) {
     res.status(400).send(error);
