0b8ac102c4086cfe7e46882c2f4b2a7f02b3d401
David Ruiz <jesuruga@gmail.com>
2024-09-21 21:37:51 -0600

    [UPDATE] Actualizar puerto a 3029

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -7,7 +7,7 @@ import router from '../adapters/routes/router';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3029;
 
 // Configurar Mongoose
 mongoose.connect(process.env.MONGODB_URI || '')
